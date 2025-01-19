cd0eb42e075724cbc360ae15197011e08e764e64
AnshChoudhary <142714177+Ansh-Choudhary-1@users.noreply.github.com>
2025-01-19 20:40:22 +0530

    Update useCurrencyInfo.js
    updated currency convertor api

--- src/Hooks/useCurrencyInfo.js
+++ src/Hooks/useCurrencyInfo.js
@@ -13,7 +13,7 @@ function useCurrencyInfo(currency) {
     let[data,setData] = useState({});
   
     useEffect(()=>{
-  fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@${date}/v1/currencies/${currency}.json`)
+  fetch(`https://${date}.currency-api.pages.dev/v1/currencies/${currency}.json`)
   .then((res)=>res.json())
   .then((res)=>setData(res[currency]));
 },[currency])
